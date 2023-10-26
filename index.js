c554791a9a5258ace5cb7c0347f562d8c0be7c1f
shambhavik24 <95088358+shambhavik24@users.noreply.github.com>
2023-10-26 20:03:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    validateInputs();
+});
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
+}
+
+const setSuccess = element => {
+    const inputControl = element;
---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+$servername = "localhost";
+$username = "your_username";
+$password = "your_password";
+$dbname = "your_database";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+$confirm_password = $_POST['confirm_password'];
+
+if ($password !== $confirm_password) {
+    die("Passwords do not match.");
+}
+
+
+$sql = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Registration successful!";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
